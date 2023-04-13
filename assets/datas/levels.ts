d754340ab018d638505a2837891540ea8042f0f6
Iglesias7 <iglesiaschendjou.isbm@gmail.com>
2023-04-13 08:58:45 +0200

    Auto-generated commit [ci skip]

---
+++ assets/datas/levels.ts
@@ -0,0 +1,92 @@
+export const levels = {
+  list: [
+    {
+      "id": 0,
+      "name": "3ieme",
+      "uaas": [
+        {
+          "id": 0,
+          "name":"UAA0",
+        },
+        {
+          "id": 1,
+          "name":"UAA1",
+        },
+        {
+          "id": 2,
+          "name":"UAA2",
+        },
+        {
+          "id": 3,
+          "name":"UAA3",
+        }
+      ]
+    },
+    {
+      "id": 1,
+      "name": "4ieme",
+      "uaas": [
+        {
+          "id": 0,
+          "name":"UAA4",
+        },
+        {
+          "id": 1,
+          "name":"UAA5",
+        },
+        {
+          "id": 2,
+          "name":"UAA6",
+        },
+        {
+          "id": 3,
+          "name":"UAA7",
+        }
+      ]
+    },
+    {
+      "id": 2,
+      "name": "5ieme",
+      "uaas": [
+        {
+          "id": 0,
+          "name":"UAA8",
+        },
+        {
+          "id": 1,
+          "name":"UAA9",
+        },
+        {
+          "id": 2,
+          "name":"UAA10",
+        },
+        {
+          "id": 3,
+          "name":"UAA11",
+        }
+      ]
+    },
+    {
+      "id": 3,
+      "name": "6ieme",
+      "uaas": [
+        {
+          "id": 0,
+          "name":"UAA12",
+        },
+        {
+          "id": 1,
+          "name":"UAA1",
+        },
+        {
+          "id": 2,
+          "name":"UAA2",
+        },
+        {
+          "id": 3,
+          "name":"UAA3",
+        }
+      ]
+    }
+  ]
+}
